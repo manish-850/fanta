b7d10c987b29e3d35d998293b9be505660844b49
Manish <br10234060526@gmail.com>
2025-08-06 14:33:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,95 +1,96 @@
 gsap.registerPlugin(ScrollTrigger);
 
-var tl = gsap.timeline({
+document.addEventListener("DOMContentLoaded", (event) => {
+  var tl = gsap.timeline({
     scrollTrigger:{
         trigger:"#page2",
         start:"0% 95%",
         end:"50% 50%",
-        scrub:true,
+        scrub:1,
     }
 });
 
 tl.to("#fanta",{
-    top:"121%",
-    left:"0%",
-    ease: "power2.out",
+    top:"120%",
+    left:"20%",
+    ease:"power1.out",
 },"anim");
 
 tl.to("#half-orange",{
     top:"160%",
     left:"22%",
-    ease: "power2.out",
-},"anim");
+    ease: "power1.out",
+},"anim+=0.1");
 
 tl.to("#oranges",{
     width:"22%",
-    top:"150%",
+    bottom:"-100%",
     right:"5%",
-    ease: "power2.out",
+    ease: "power1.out",
 },"anim");
 
 tl.to("#leaf",{
     rotate:"310deg",
     top:"110%",
     left:"80%",
-    ease: "power2.out",
+    ease: "power1.out",
 },"anim");
 
 tl.to("#leaf2",{
     rotate:"-270deg",
-    top:"105%",
+    bottom:"-30%",
     right:"85%",
-    ease: "power2.out",
-},"anim");
-
-
+    ease: "power1.out",
+},"anim+=0.1");
 
 var tl2 = gsap.timeline({
     scrollTrigger:{
         trigger:"#page3",
         start:"0% 95%",
         end:"20% 50%",
-        scrub:true,
-        // markers:true,
+        scrub:1,
     }
 });
 
 tl2.to("#fanta",{
-    width:"30%",
-    top:"220%",
-    left:"35%",
-    ease: "power2.out",
-},"anime2");
+    width:"31%",
+    top:"215%",
+    left:"50%",
+    ease: "power1.out",
+},"anim2");
 
 tl2.to("#half-orange",{
-    width:"17%",
-    top:"205%",
-    left:"41.5%",
-    ease: "power2.out",
-},"anime2");
+    top:"204%",
+    left:"42%",
+    ease: "power1.out",
+},"anim2+=0.1");
+ 
+tl2.from("#main #page3 .card .main-img1",{
+    rotate:-90,
+    top: "0%",
+    left: "-100%",
+    ease: "power1.out",
+},"anim2");
 
-// tl2.from(".main-img1",{
-//     rotate:"-90deg",
-//     left:"-100%",
-//     ease: "power2.out",
-// },"anime2");
+tl2.from("#main #page3 .card .bg-img1",{
+    rotate:-90,
+    top: "-20%",
+    left: "-100%",
+    ease: "power1.out",
+},"anim2+=0.1");
 
-// tl2.from(".bg-img1",{
-//     rotate:"-90deg",
-//     left:"-100%",
-//     ease: "power2.out",
-// },"anime2");
+tl2.from("#main #page3 .card .main-img3",{
+    rotate:90,
+    top: "-2%",
+    left: "100%",
+    ease: "power1.out",
+},"anim2");
 
-// tl2.from(".main-img3",{
-//     rotate:"90deg",
-//     left:"100%",
-//     ease: "power2.out",
-// },"anime2");
+tl2.from("#main #page3 .card .bg-img3",{
+    rotate:90,
+    top: "-28%",
+    left: "100%",
+    ease: "power1.out",
+},"anim2+=0.1");
 
-// tl2.from(".bg-img3",{
-//     rotate:"90deg",
-//     left:"100%",
-//     ease: "power2.out",
-
-// },"anime2");
-
+});
